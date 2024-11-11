1e2619a21b1d920b02d92887471633bf81cf6af7
Tariel Khalvashi <taro.khalvashi1225@gmail.com>
2024-11-11 15:56:52 +0400

    NetworkingModule

--- Package.swift
+++ Package.swift
@@ -1,21 +1,21 @@
-// swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+    // swift-tools-version: 6.0
+    // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-import PackageDescription
+    import PackageDescription
 
-let package = Package(
-    name: "NetworkingForNews",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "NetworkingForNews",
-            targets: ["NetworkingForNews"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "NetworkingForNews"),
+    let package = Package(
+        name: "NetworkingForNews",
+        products: [
+            // Products define the executables and libraries a package produces, making them visible to other packages.
+            .library(
+                name: "NetworkingForNews",
+                targets: ["NetworkingForNews"]),
+        ],
+        targets: [
+            // Targets are the basic building blocks of a package, defining a module or a test suite.
+            // Targets can depend on other targets in this package and products from dependencies.
+            .target(
+                name: "NetworkingForNews"),
 
-    ]
-)
+        ]
+    )
--- Sources/NetworkingForNews/NetworkingForNews.swift
+++ Sources/NetworkingForNews/NetworkingForNews.swift
@@ -1,2 +1,78 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public struct Source: Codable {
+    public let id: String?
+    public let name: String
+}
+
+public struct NewsResponseData: Codable {
+    public let status: String
+    public let totalResults: Int
+    public let articles: [NewsArticle]?
+    
+}
+
+public struct NewsArticle: Codable {
+    public let source: Source
+    public let author: String?
+    public let title: String
+    public let description: String?
+    public let url: String
+    public let urlToImage: String?
+    public let publishedAt: String
+    public let content: String?
+}
+
+public protocol NetworkingProtocol {
+    func fetchNewsArticles(completion: @escaping @Sendable (Result<[NewsArticle], Error>) -> Void)
+}
+
+
+public class NetworkingForNews: NetworkingProtocol {
+    
+    private let apiKey = "9090160e5ec147ca97b94502994ebc28"
+    
+    public init() {}
+    
+    public func fetchNewsArticles(completion: @escaping @Sendable (Result<[NewsArticle], Error>) -> Void) {
+        let urlString = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=\(apiKey)"
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    completion(.failure(NSError(domain: "No Data", code: 404, userInfo: nil)))
+                }
+                return
+            }
+            
+            do {
+                let newsResponse = try JSONDecoder().decode(NewsResponseData.self, from: data)
+                if let articles = newsResponse.articles {
+                    DispatchQueue.main.async {
+                        completion(.success(articles))
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        completion(.failure(NSError(domain: "No Articles", code: 404, userInfo: nil)))
+                    }
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }.resume()
+    }
+    
+}
